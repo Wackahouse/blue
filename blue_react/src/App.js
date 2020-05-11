39cc18c171664b9ef1a49f64a1f3c87f9336cc97
bgurneet <sbgurneet@gmail.com>
2020-05-11 12:11:12 +0400

    made the top navbar links and pages

--- blue_react/src/App.js
+++ blue_react/src/App.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container'
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import 'jquery';
 
 function Home() {
   return (
@@ -13,10 +14,34 @@ function Home() {
   )
 }
 
-function Room() {
+function Leaderboard() {
   return (
     <div className="content container">
-      <h1>Room Page</h1>
+      <h1>Leaderboard Page</h1>
+    </div>
+  )
+}
+
+function Help() {
+  return (
+    <div className="content container">
+      <h1>Help page</h1>
+    </div>
+  )
+}
+
+function Contact() {
+  return (
+    <div className="content container">
+      <h1>Contact us Page</h1>
+    </div>
+  )
+}
+
+function Login() {
+  return (
+    <div className="content container">
+      <h1>Login/Signup</h1>
     </div>
   )
 }
@@ -27,7 +52,10 @@ function App() {
       <Navbar />
       <Switch>
         <Route path='/' component={Home} exact/>
-        <Route path='/Room' component={Room} exact/>
+        <Route path='/leaderboard' component={Leaderboard} exact/>
+        <Route path='/contact' component={Contact} exact/>
+        <Route path='/help' component={Help} exact/>
+        <Route path='/login' component={Login} exact/>
         <Redirect to='/' />
       </Switch>
     </BrowserRouter>
--- blue_react/src/components/Navbar.js
+++ blue_react/src/components/Navbar.js
@@ -3,9 +3,30 @@ import React, { Component } from 'react';
 class Navbar extends Component {
 	render() {
 		return (
-			<nav className="navbar navbar-expand-md bg-primary navbar-dark fixed-top">
-      			<a className="navbar-brand" href="/">Blue</a>
-    		</nav>
+			<nav className="navbar navbar-expand-md justify-content-between bg-primary navbar-dark fixed-top">
+		      <a className="navbar-brand" href="/">Blue</a>
+		      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+		        <span className="navbar-toggler-icon"></span>
+		      </button>
+		      <div className="collapse navbar-collapse" id="collapsibleNavbar">
+		        <ul className="navbar-nav">
+		          <li className="nav-item">
+		            <a className="nav-link" href="leaderboard">Leaderboard</a>
+		          </li>
+		          <li className="nav-item">
+		            <a className="nav-link" href="help">Help</a>
+		          </li>
+		          <li className="nav-item">
+		            <a className="nav-link" href="contact">Contact</a>
+		          </li>
+		        </ul>
+		        <ul className="navbar-nav ml-auto">
+		          <li className="nav-item">
+		            <a className="nav-link" href="login">Login/Signup</a>
+		          </li>
+		        </ul>
+		      </div>
+		    </nav>
     	);
 	}
 }
